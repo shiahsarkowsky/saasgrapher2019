a65633508a29158805c740866abd091b0dbbffa0
shiahsarkowsky <31865846+shiahsarkowsky@users.noreply.github.com>
2019-03-05 12:23:47 -0800

    Add files via upload

---
+++ setup.js
@@ -0,0 +1,80 @@
+//Shiah's setup script for quao solver and grapher. 
+//canvas set up and graph paper
+
+//global variables
+
+var k = 10, ctx, a, b , c, vX, vY, w, h;
+
+function init() {
+  canvas = document.getElementById("mycanvas");
+  ctx = canvas.getContext("2d");
+  w = canvas.width = 600;
+  h = canvas.height = 400;
+  console.log('canvas is loaded into context',w);
+
+  graphpaper();
+}
+
+function graphpaper() {
+  // the x and y axis drawn
+  ctx.lineWidth=2;
+  ctx.beginPath();
+  ctx.moveTo(w/2,0);
+  ctx.lineTo(w/2,h);
+  ctx.stroke();
+
+  ctx.beginPath(); 
+  ctx.moveTo(0,h/2);
+  ctx.lineTo(w,h/2);
+  ctx.stroke();
+
+  // thin line with a 50% opacity using rgba() 
+  ctx.lineWidth=1;
+  ctx.strokeStyle="rgba(0,0,100,.2)";
+
+  //using the direct variation constant, k
+  //  here are the vertical and horizontal lines
+
+  //vertical lines
+  for (i=0; i<w/(2*k); i++) {
+    // middle out to ensure graph paper is centered over axis
+    ctx.beginPath();
+    ctx.moveTo( w/2+i*k, 0 );
+    ctx.lineTo( w/2+i*k, h );
+    ctx.stroke();
+
+
+	} // end for loop
+
+
+  for (i=0; i<w/(2*k); i++) {
+    // middle out to ensure graph paper is centered over axis
+    ctx.beginPath();
+    ctx.moveTo( w/2-i*k, 0 );
+    ctx.lineTo( w/2-i*k, h );
+    ctx.stroke();
+//end of vertical lines
+
+	} // end for loop
+
+//horizontel lines
+  for (i=0; i<h/(2*k); i++) {
+    // middle out to ensure graph paper is centered over axis
+    ctx.beginPath();
+    ctx.moveTo( 0, h/2 + i*k);
+    ctx.lineTo( w, h/2 + i*k);
+    ctx.stroke();
+
+
+	} // end for loop	
+ for (i=0; i<h/(2*k); i++) {
+    // middle out to ensure graph paper is centered over axis
+    ctx.beginPath();
+    ctx.moveTo( 0, h/2 - i*k);
+    ctx.lineTo( w, h/2 - i*k);
+    ctx.stroke();
+
+
+	} // end for loop	
+//end of horizontel lines
+}

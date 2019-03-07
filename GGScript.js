497a2dd13dfe163fcfdf3ba166b1f6115a0fb2f2
shiahsarkowsky <31865846+shiahsarkowsky@users.noreply.github.com>
2019-03-07 09:16:25 -0800

    Add files via upload

--- GGScript.js
+++ GGScript.js
@@ -18,7 +18,7 @@ function Ggraph () {
     ctx.lineTo(scaledX(nx),scaledY(plugX2(nx)));
     ctx.stroke();
   }
-  $("#yint").text("Y intercept is at (0," + plugX2(0)+")");
+  $("#yInt").text("Y intercept is at (0," + plugX2(0)+")");
   ctx.beginPath();
   ctx.arc(scaledX(0),scaledY(plugX2(0)),5,0,6.28);
   ctx.fill();
@@ -26,11 +26,21 @@ function Ggraph () {
 
 function zoomIn() {
  k=k+3;
- resetCanvas();
+ ctx.clearRect(0,0,w,h);
+ graphpaper();
+ Ggraph();
+}
+
+function reset2() {
+ ctx.clearRect(0,0,w,h)
+ init();
+ graphpaper();
 }
 function zoomOut() {
  k=k-3;
- resetCanvas();
+ ctx.clearRect(0,0,w,h)
+ graphpaper();
+ Ggraph();
 }
 
 function scaledX(x) {
@@ -45,49 +55,4 @@ function scaledY(y) {
 function plugX2(x) {
  y = eval(f);
  return y;
-}
-function init() {
-  canvas = document.getElementById("mycanvas");
-  ctx = canvas.getContext("2d");
-  w = canvas.width = 600;
-  h = canvas.height = 400;
-  console.log('canvas is loaded into context',w);
-
-  graphpaper();
-}
-
-function graphpaper() {
-  // the x and y axis drawn
-  ctx.lineWidth=2;
-  ctx.beginPath();
-  ctx.moveTo(w/2,0);
-  ctx.lineTo(w/2,h);
-  ctx.stroke();
-
-  ctx.beginPath(); 
-  ctx.moveTo(0,h/2);
-  ctx.lineTo(w,h/2);
-  ctx.stroke();
-
-  // thin line with a 50% opacity using rgba() 
-  ctx.lineWidth=1;
-  ctx.strokeStyle="rgba(0,0,100,.2)";
-
-  //using the direct variation constant, k
-  //  here are the vertical and horizontal lines
-
-  //vertical lines
-  for (i=0; i<w/(2*k); i++) {
-    // middle out to ensure graph paper is centered over axis
-    ctx.beginPath();
-    ctx.moveTo( w/2+i*k, 0 );
-    ctx.lineTo( w/2+i*k, h );
-    ctx.stroke();
-
-
-	} // end for loop
-  
-function reset() {
- init();
- Ggraph();
 }

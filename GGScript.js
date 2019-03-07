e607a4c68a61863f28b75b61c4d029721ea08d89
shiahsarkowsky <31865846+shiahsarkowsky@users.noreply.github.com>
2019-03-07 08:56:39 -0800

    Update GGScript.js

--- GGScript.js
+++ GGScript.js
@@ -28,11 +28,6 @@ function zoomIn() {
  k=k+3;
  resetCanvas();
 }
-
-function reset() {
- init();
- Ggraph();
-}
 function zoomOut() {
  k=k-3;
  resetCanvas();
@@ -50,4 +45,49 @@ function scaledY(y) {
 function plugX2(x) {
  y = eval(f);
  return y;
+}
+function init() {
+  canvas = document.getElementById("mycanvas");
+  ctx = canvas.getContext("2d");
+  w = canvas.width = 600;
+  h = canvas.height = 400;
+  console.log('canvas is loaded into context',w);
+
+  graphpaper();
+}
+
+function graphpaper() {
+  // the x and y axis drawn
+  ctx.lineWidth=2;
+  ctx.beginPath();
+  ctx.moveTo(w/2,0);
+  ctx.lineTo(w/2,h);
+  ctx.stroke();
+
+  ctx.beginPath(); 
+  ctx.moveTo(0,h/2);
+  ctx.lineTo(w,h/2);
+  ctx.stroke();
+
+  // thin line with a 50% opacity using rgba() 
+  ctx.lineWidth=1;
+  ctx.strokeStyle="rgba(0,0,100,.2)";
+
+  //using the direct variation constant, k
+  //  here are the vertical and horizontal lines
+
+  //vertical lines
+  for (i=0; i<w/(2*k); i++) {
+    // middle out to ensure graph paper is centered over axis
+    ctx.beginPath();
+    ctx.moveTo( w/2+i*k, 0 );
+    ctx.lineTo( w/2+i*k, h );
+    ctx.stroke();
+
+
+	} // end for loop
+  
+function reset() {
+ init();
+ Ggraph();
 }
